f57446be544b3169cae43051cbe41b1d643054c9
NICOLAS ORLANDO <norlando@alumno.unlam.edu.ar>
2021-05-22 12:55:01 -0300

    Agrego UserController.

---
+++ Nokiu.WebApplication/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Nokiu.WebApplication.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
